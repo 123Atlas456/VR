75d9309ea606069f89ca66a0815aae33f87cd964
123Atlas456 <dear20090122@gmail.com>
2024-11-22 11:53:57 +0800

    ㄍˋ

---
+++ cave/data.js
@@ -0,0 +1,41 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360_degree_panorama_minecraft_cave_render_by_foxelbeton_dfm3q5u-pre",
+      "name": "360_degree_panorama_minecraft_cave_render_by_foxelbeton_dfm3q5u-pre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 316,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.8997531703615351,
+          "pitch": 0.22291772363219664,
+          "title": "這是熔岩",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
