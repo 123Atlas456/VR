11be33a6696afc855ccecc12988062f9b54a1067
123Atlas456 <dear20090122@gmail.com>
2025-01-03 11:01:58 +0800

    Create dta.js

---
+++ shader-files/dta.js
@@ -0,0 +1,47 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "光影",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.7751900776642433,
+          "pitch": 0.3167220992480271,
+          "title": "這是橡木原木",
+          "text": "生存必需品<div><br></div>"
+        },
+        {
+          "yaw": -0.47080112314138844,
+          "pitch": -1.0648574224173721,
+          "title": "這是minecraft特性",
+          "text": "牛頓很崩潰"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
